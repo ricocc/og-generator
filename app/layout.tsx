82b59925d245113b779f99d35f42ef50d2fffcc5
Ricocc <ricocc@qq.com>
2025-01-27 20:23:41 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
 import "@/styles/globals.css"
 
-import type { Metadata } from "next"
 import { TemplateStoreProvider } from "@/providers/template-store-provider"
 import { GeistMono } from "geist/font/mono"
 import { GeistSans } from "geist/font/sans"
+import type { Metadata } from "next"
 
-import { siteConfig } from "@/config/site"
-import { Separator } from "@/components/ui/separator"
-import { Toaster } from "@/components/ui/sonner"
-import Footer from "@/components/footer/Footer"
-import { ThemeProvider } from "@/components/theme-provider"
 import GoogleAdsense from "@/app/GoogleAdsense"
 import GoogleAnalytics from "@/app/GoogleAnalytics"
+import Footer from "@/components/footer/Footer"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Separator } from "@/components/ui/separator"
+import { Toaster } from "@/components/ui/sonner"
+import { siteConfig } from "@/config/site"
+
 
 export const metadata: Metadata = {
   ...siteConfig,
